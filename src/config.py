ada9553871f9552a56aae31b17a6c46d9bab55fd
Leonardo Maydana <leomay282@gmail.com>
2024-05-11 19:11:20 -0300

    Preprocessing1

--- src/config.py
+++ src/config.py
@@ -6,5 +6,4 @@ os.makedirs(DATASET_ROOT_PATH, exist_ok=True)
 
 DATASET_TRAIN = str(Path(DATASET_ROOT_PATH) / "trans_data.parquet")
 DATASET_TRAIN_URL = (
-    "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-05.parquet"
-)
+    "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-05.parquet")
